212348f9b099f2dda12366a473bb72a65d95d9fd
Jue906D <3090848504@qq.com>
2026-01-31 13:26:27 +0800

    feat: sound manager

---
+++ Rabbit-the-last-Mask/Assets/Script/Manager/AudioManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using RotaryHeart.Lib.SerializableDictionary;
+using Script.Tools;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Serialization;
+
+namespace Script.Manager
+{
+    public class AudioManager : SingletonMonoBehaviour<AudioManager>
+    {
+        [SerializeField]
+        public ClipDict bgmDict;
+        public AudioSource bgmSource;
+        public float bgmFadeTime = 1f; 
+        
+        [SerializeField]
+        public ClipDict sfxDict;
+        public AudioSource[] sfxSources;      // 10个SFX通道
+        private int lastUsedChannel = -1; 
+
+        [Header("混音")] public AudioMixer audioMixer;
+        
+        [Serializable]
+        public class ClipDict : SerializableDictionaryBase<string, AudioClip>{};
+        [Serializable]
+        public class SourceDict : SerializableDictionaryBase<string, AudioSource>{};
+        
+        AudioSource GetAvailableChannel()
+        {
+            for (int i = 0; i < sfxSources.Length; i++)
+            {
+                if (!sfxSources[i].isPlaying)
+                    return sfxSources[i];
+            }
+            lastUsedChannel = (lastUsedChannel + 1) % sfxSources.Length;
+            return sfxSources[lastUsedChannel];
+        }
+        public void PlaySfx(AudioClip clip, Vector3 position = default)
+        {
+            AudioSource channel = GetAvailableChannel();
+        
+            // 设置参数
+            channel.transform.position = position;
+            channel.volume = 1f;
+            channel.clip = clip;
+            channel.Play();
+        }
+
+        public void PlayBGM(string clipName)
+        {
+            bgmSource.clip = bgmDict[clipName];
+            bgmSource.volume = 1f;
+            bgmSource.Play();
+        }
+
+
+        public void Awake()
+        {
+            bgmSource.loop = true;
+            bgmSource.playOnAwake = false;
+            
+            for (int i = 0; i < sfxSources.Length; i++)
+            {
+
+                sfxSources[i].playOnAwake = false;
+                sfxSources[i].volume = 1f;
+            }
+        }
+    }
+}
